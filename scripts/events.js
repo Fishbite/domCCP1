a7a3b387d053d0d6b0ce72ea0999b41d39e7e0c3
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-05-18 12:36:08 +0100

    update buttonClick()

--- scripts/events.js
+++ scripts/events.js
@@ -16,4 +16,8 @@ function buttonClick(e) {
   // document.querySelector(".form-container").style.backgroundColor = "#f4f4f4";
   console.log(e.target);
   console.log(e.target.id);
+  console.log(e.target.class);
+  console.log(e.target.classList); // returns DOMTokenList
+  let output = document.getElementById("output");
+  output.innHTML = "<h3>" + e.target.id + "</h3>";
 }
